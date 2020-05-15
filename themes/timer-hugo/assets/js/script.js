9912ea74419347b64dcd7df21424d52b0434a220
lorenarm96 <lure-rm96@hotmail.com>
2020-05-15 16:36:47 +0200

    Services done

--- themes/timer-hugo/assets/js/script.js
+++ themes/timer-hugo/assets/js/script.js
@@ -174,57 +174,57 @@ $(".fancybox").fancybox({
 });
 
 //Appear text
-//jQuery(document).ready(function() {
-//    $('.section')                          // Filter: .section
-//        .find('.description')              // Filter: .section .description
-//            .hide()                        // Hide all
-//            .end()                         // End current filter
-//        .find('.title_section3')                  // Filter: .section
-//            .click( function(){            // Set the onclick action
-//                $(this).siblings('.description')  // Filter: .section .title (sibling of .description)
-//                .slideToggle();            // Toggle visibility
-//            });
-//});
+jQuery(document).ready(function() {
+    $('.section')                                 // Filter: .section
+        .find('.description')                     // Filter: .section .description
+            .hide()                               // Hide all
+            .end()                                // End current filter
+        .find('.title_section3')                  // Filter: .section
+            .click( function(){                   // Set the onclick action
+                $(this).siblings('.description')  // Filter: .section .title (sibling of .description)
+                .slideToggle();                   // Toggle visibility
+            });
+});
 
-$(document).ready(function() {
-    (function() {
-      var showChar = 120;
-      var ellipsestext = "...";
-  
-      $(".truncate").each(function() {
-        var content = $(this).html();
-        if (content.length > showChar) {
-          var c = content.substr(0, showChar);
-          var h = content;
-          var html =
-            '<div class="truncate-text" style="display:block">' +
-            c +
-            '<span class="moreellipses">' +
-            ellipsestext +
-            '&nbsp;&nbsp;<a href="" class="moreless more">more</a></span></span></div><div class="truncate-text" style="display:none">' +
-            h +
-            '<a href="" class="moreless less">Less</a></span></div>';
-  
-          $(this).html(html);
-        }
-      });
-  
-      $(".moreless").click(function() {
-        var thisEl = $(this);
-        var cT = thisEl.closest(".truncate-text");
-        var tX = ".truncate-text";
-  
-        if (thisEl.hasClass("less")) {
-          cT.prev(tX).toggle();
-          cT.slideToggle();
-        } else {
-          cT.toggle();
-          cT.next(tX).fadeToggle();
-        }
-        return false;
-      });
-      /* end iffe */
-    })();
-  
-    /* end ready */
-  });
+//$(document).ready(function() {
+//    (function() {
+//      var showChar = 120;
+//      var ellipsestext = "...";
+//  
+//      $(".truncate").each(function() {
+//        var content = $(this).html();
+//        if (content.length > showChar) {
+//          var c = content.substr(0, showChar);
+//          var h = content;
+//          var html =
+//            '<div class="truncate-text" style="display:block">' +
+//            c +
+//            '<span class="moreellipses">' +
+//            ellipsestext +
+//            '&nbsp;&nbsp;<a href="" class="moreless more">more</a></span></span></div><div class="truncate-text" style="display:none">' +
+//            h +
+//            '<a href="" class="moreless less">Less</a></span></div>';
+//  
+//          $(this).html(html);
+//        }
+//      });
+//  
+//      $(".moreless").click(function() {
+//        var thisEl = $(this);
+//        var cT = thisEl.closest(".truncate-text");
+//        var tX = ".truncate-text";
+//  
+//        if (thisEl.hasClass("less")) {
+//          cT.prev(tX).toggle();
+//          cT.slideToggle();
+//        } else {
+//          cT.toggle();
+//          cT.next(tX).fadeToggle();
+//        }
+//        return false;
+//      });
+//      /* end iffe */
+//    })();
+//  
+//    /* end ready */
+//  });
